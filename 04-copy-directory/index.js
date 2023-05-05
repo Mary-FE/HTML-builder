e33c75a3d606159ab76a4f36a0ef9f9588510ca2
Mary Z <masha.zhirova@yandex.ru>
2023-05-05 13:49:46 +0300

    feat: add solve the 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,9 @@
+const fs = require('fs');
 
+function copyDir(path, dest) {
+    fs.rm(dest, { recursive: true}, (err) => {
+        fs.cp(path, dest, {recursive: true}, (err) => {});
+    }); 
+}
+
+copyDir('./04-copy-directory/files', './04-copy-directory/files-copy');
